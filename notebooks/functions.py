74bdc6c471e57dd751e12d13d793d5112f8d5cb3
arisenn02 <arina.senn@hispeed.ch>
2026-05-13 21:46:26 +0200

    make functions of question 3 more reproducable

--- notebooks/functions.py
+++ notebooks/functions.py
@@ -1,19 +1,23 @@
 import matplotlib.pyplot as plt
-def plot_category_over_years(data,categories):
-    data = data.set_index("report_time")
+def plot_category_over_years(data,categories,time_column,category_column):
+    data = data.set_index(time_column)
     fig, ax = plt.subplots()
     colors=["steelblue","seagreen","sandybrown","mediumpurple","lightcoral","cadetblue","rosybrown","darkseagreen","tan","plum"]
     i=0
-    for category in categories:
-        filtered_data = data[data["category"]==category]
+    for cat in categories:
+        filtered_data = data[data[category_column]==cat]
         monthly_reports = filtered_data.resample("ME").size().reset_index(name="reports")
-        ax.plot(monthly_reports["report_time"], monthly_reports["reports"],color=colors[i],label = category,)
+        ax.plot(monthly_reports[time_column], monthly_reports["reports"],color=colors[i],label = category,)
         i+=1
     ax.set_xlabel("Time [Year]")
     ax.set_ylabel("Number of Reports")
     ax.set_title("Monthly Reports by Category over Years")
     ax.legend()
     plt.show()
+
+
+
+    
 
 def plot_category_over_a_year(data,categories,year):
     data = data.set_index("report_time")
